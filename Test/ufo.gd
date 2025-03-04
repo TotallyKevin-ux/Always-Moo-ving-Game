056ad69b876dfa7c37e8cac369c5dede29449940
TotallyKevin-ux <tasin4434@gmail.com>
2025-03-04 01:22:18 -0500

    Update ufo.gd

--- Test/ufo.gd
+++ Test/ufo.gd
@@ -3,24 +3,13 @@ extends CharacterBody2D
 @export var player:Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
+var active = false
+
 const SPEED = 150.0
-
-
 
 func _physics_process(_delta: float):
 	var next_path_pos := nav_agent.get_next_path_position()
 	var direction := global_position.direction_to(next_path_pos)
-<<<<<<< Updated upstream
-	velocity = direction * SPEED
-	move_and_slide()
-
-
-#func path():
-	#nav_agent.target_position = player.global_position
-#
-#func _on_timer_timeout():
-	#path()
-=======
 	if active == true: 
 		velocity = direction * SPEED
 	else:
@@ -32,13 +21,13 @@ func path():
 
 func _on_timer_timeout():
 	path()
-
-
 func _on_death_timer_timeout() -> void:
 	pass # Replace with function body.
-
 
 func _on_detection_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
 		active = true
->>>>>>> Stashed changes
+
+func _on_detection_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Player"):
+		active = true
